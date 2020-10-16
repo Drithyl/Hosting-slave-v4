caf8b18601d771d6b7278c780b9a442b3f36dd34
Hugo Bujan Prevot <hbp1@student.london.ac.uk>
2020-10-16 12:38:09 -0400

    Refactored backup scripts

--- backup_script.js
+++ backup_script.js
@@ -1,139 +1,105 @@
-require("./prototype_functions.js");
+
 const fs = require("fs");
+const fsp = require("fs").promises;
 const config = require("./config.json");
 const rw = require("./reader_writer.js");
-const timerParser = require("./timer_parser.js");
 const preexecRegex = new RegExp("^\\-\\-preexec$", "i");
 const postexecRegex = new RegExp("^\\-\\-postexec$", "i");
+const statusDump = require("./dom5/status_dump_wrapper.js");
 const extensionsToBackupRegex = new RegExp("(\.2h)|(\.trn)|(ftherlnd)$", "i");
 
+
+var fetchedStatusDump;
 var gameName = process.argv[2];
 var type = process.argv[3];
 var source = `${config.dom5DataPath}/savedgames/${gameName}`;
 var target = `${config.dataFolderPath}/backups`;
-var turn;
-var turnInfo;
+
 
 rw.log(["backup"], `Backup type ${type} for ${gameName} starting.`);
 
+
 if (gameName == null)
-{
-  rw.log(["error", "backup"], true, `No game name argument received.`);
-}
+    return rw.log(["error", "backup"], true, `No game name argument received.`);
 
 if (preexecRegex.test(type) === true)
-{
-  target += `${config.latestTurnBackupDirName}/${gameName}`;
-}
+    target += `${config.latestTurnBackupDirName}/${gameName}`;
 
 else if (postexecRegex.test(type) === true)
-{
-  target += `${config.newTurnsBackupDirName}/${gameName}`;
-}
+    target += `${config.newTurnsBackupDirName}/${gameName}`;
 
 else
-{
-  rw.log(["error", "backup"], true, `Backup type received is invalid; expected --preexec or --postexec: ${type}`);
-  return;
-}
+    return rw.log(["error", "backup"], true, `Backup type received is invalid; expected --preexec or --postexec: ${type}`);
 
-try
-{
-  turnInfo = timerParser.getTurnInfoSync(gameName);
-}
 
-catch(err)
+statusDump.fetchStatusDump(gameName)
+.then((statusDumpWrapper) =>
 {
-  rw.log(["error", "backup"], true, `Statuspage reading Error:\n\n${err.message}`);
-  return;
-}
-
-if (turnInfo == null)
+    //statusdump doesn't update fast enough right after turn processes,
+    //so turn number will have to be manually increased
+    statusDumpWrapper.turnNbr++;
+    fetchedStatusDump = statusDumpWrapper;
+    return _createDirectories(`${target}/Turn ${turnInfo.turn}`);
+})
+.then(() => fsp.readdir(source))
+.then((filenames) =>
 {
-  rw.log(["error", "backup"], true, `Statuspage could not be found, cannot archive turn.`);
-  return;
-}
-
-//statuspages don't update fast enough to give the new turn number right after
-//the turn processes, therefore add 1 to it
-if (postexecRegex.test(type) === true)
-{
-  turnInfo.turn++;
-}
-
-try
-{
-  createDirsSync(`${target}/Turn ${turnInfo.turn}`);
-}
-
-catch(err)
-{
-  rw.log(["error", "backup"], true, `Dirs could not be created for backups:\n\n${err.message}`);
-  return;
-}
-
-try
-{
-  let filenames = fs.readdirSync(source);
-
-  filenames.forEach((filename) =>
-  {
-    if (extensionsToBackupRegex.test(filename) === true)
+    filenames.forEachPromise((filename, index, nextPromise) =>
     {
-      let data = fs.readFileSync(`${source}/${filename}`);
-      fs.writeFileSync(`${target}/Turn ${turnInfo.turn}/${filename}`, data);
-    }
-  });
-
-  //delete previous backups according to the configuration
-  if (turnInfo.turn > config.nbrOfTurnsBackedUp)
-  {
-    if (fs.existsSync(`${target}/Turn ${turnInfo.turn - config.nbrOfTurnsBackedUp}`) === true)
-    {
-      rw.atomicRmDir(`${target}/Turn ${turnInfo.turn - config.nbrOfTurnsBackedUp}`, function(err)
-      {
-        if (err)
+        return Promise.resolve()
+        .then(() =>
         {
-          rw.log(["error", "backup"], `atomicRmDir() Error: ${err.message}`);
+            if (extensionsToBackupRegex.test(filename) === true)
+            {
+                return fsp.readFile(`${source}/${filename}`)
+                .then((buffer) => fsp.writeFile(`${target}/Turn ${turnInfo.turn}/${filename}`, buffer));
+            }
+
+            else return Promise.resolve();
+        })
+        .then(() => nextPromise());
+    })
+    .then(() =>
+    {
+        //delete previous backups according to the configuration
+        if (turnInfo.turn > config.nbrOfTurnsBackedUp)
+        {
+            if (fs.existsSync(`${target}/Turn ${turnInfo.turn - config.nbrOfTurnsBackedUp}`) === false)
+            return Promise.resolve();
+
+            return rw.atomicRmDir(`${target}/Turn ${turnInfo.turn - config.nbrOfTurnsBackedUp}`);
         }
-      });
-    }
-  }
-}
 
-catch(err)
+        else return Promise.resolve();
+    });
+});
+
+
+function _createDirectories(target)
 {
-  rw.log(["error", "backup"], `FS Error: ${err.message}`);
-}
+    //Linux base paths begin with / so ignore the first empty element
+    if (target.indexOf("/") === 0)
+        target = target.slice(1);
 
-function createDirsSync(target)
-{
-  //Linux base paths begin with / so ignore the first empty element
-  if (target.indexOf("/") === 0)
-  {
-    target = target.slice(1);
-  }
+    var directories = target.split("/");
+    var currentPath = directories.shift();
 
-  let dirs = target.split("/");
-  let currentPath = dirs.shift();
-
-  if (process.platform === "linux")
-  {
-    currentPath = `/${currentPath}`;
-  }
-
-  if (fs.existsSync(currentPath) === false)
-  {
-    throw new Error(`The base path ${currentPath} specified for the backup target does not exist.`);
-  }
-
-  dirs.forEach((dir) =>
-  {
-    currentPath += `/${dir}`;
+    if (process.platform === "linux")
+        currentPath = `/${currentPath}`;
 
     if (fs.existsSync(currentPath) === false)
+        throw new Error(`The base path ${currentPath} specified for the backup target does not exist.`);
+
+    return directories.forEachPromise((dir, index, nextPromise) =>
     {
-      fs.mkdirSync(currentPath);
-    }
-  });
+        currentPath += `/${dir}`;
+
+        if (fs.existsSync(currentPath) === false)
+        {
+            return fsp.mkdir(currentPath)
+            .then(() => nextPromise());
+        }
+            
+        else return nextPromise();
+    });
 }
