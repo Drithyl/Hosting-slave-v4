878d0ea5731d2e8076e51e3e8ba93d73d4031930
Drithyl <hugo.bujan@gmail.com>
2021-01-04 23:29:53 -0500

    Refactored and fixed turn backups

--- backup_script.js
+++ backup_script.js
@@ -3,97 +3,114 @@ const fs = require("fs");
 const fsp = require("fs").promises;
 const config = require("./config.json");
 const rw = require("./reader_writer.js");
-const socketWrapper = require("./socket_wrapper.js");
+const cleaner = require("./unused_files_cleaner.js");
+const statusDump = require("./dom5/status_dump_wrapper.js");
+
 const preexecRegex = new RegExp("^\\-\\-preexec$", "i");
 const postexecRegex = new RegExp("^\\-\\-postexec$", "i");
-const statusDump = require("./dom5/status_dump_wrapper.js");
 const extensionsToBackupRegex = new RegExp("(\.2h)|(\.trn)|(ftherlnd)$", "i");
 
+const gameName = process.argv[2];
+const type = process.argv[3];
+const savedgamesPath = `${config.dom5DataPath}/savedgames/${gameName}`;
 
+
+var targetBackupDir = `${config.dataFolderPath}/backups/${gameName}`;
 var fetchedStatusDump;
-var gameName = process.argv[2];
-var type = process.argv[3];
-var source = `${config.dom5DataPath}/savedgames/${gameName}`;
-var target = `${config.dataFolderPath}/backups`;
 
 
 rw.log(["backup"], `Backup type ${type} for ${gameName} starting.`);
 
 
 if (gameName == null)
-    return rw.log(["error", "backup"], true, `No game name argument received.`);
+    return rw.log(["error", "backup"], `No game name argument received.`);
 
 if (preexecRegex.test(type) === true)
-    target += `${config.latestTurnBackupDirName}/${gameName}`;
+    targetBackupDir += `/${config.preHostTurnBackupDirName}`;
 
 else if (postexecRegex.test(type) === true)
-{
-    console.log(`${gameName}: new turn!`);
-    target += `${config.newTurnsBackupDirName}/${gameName}`;
-    socketWrapper.emit("NEW_TURN", { gameName: gameName });
-}
+    targetBackupDir += `/${config.newTurnsBackupDirName}`;
 
-else
-    return rw.log(["error", "backup"], true, `Backup type received is invalid; expected --preexec or --postexec: ${type}`);
+else return rw.log(["error", "backup"], `Invalid backup type received; expected --preexec or --postexec: '${type}'`);
+
+
+rw.log(["backup"], `Fetching statusdump...`);
 
 
 statusDump.fetchStatusDump(gameName)
 .then((statusDumpWrapper) =>
 {
-    //statusdump doesn't update fast enough right after turn processes,
-    //so turn number will have to be manually increased
-    statusDumpWrapper.turnNbr++;
     fetchedStatusDump = statusDumpWrapper;
-    return _createDirectories(`${target}/Turn ${turnInfo.turn}`);
-})
-.then(() => fsp.readdir(source))
-.then((filenames) =>
-{
-    filenames.forEachPromise((filename, index, nextPromise) =>
-    {
-        return Promise.resolve()
-        .then(() =>
-        {
-            if (extensionsToBackupRegex.test(filename) === true)
-            {
-                return fsp.readFile(`${source}/${filename}`)
-                .then((buffer) => fsp.writeFile(`${target}/Turn ${turnInfo.turn}/${filename}`, buffer));
-            }
+    const tmpTurnFilePath = `${config.tmpDownloadPath}/${gameName}`;
+    rw.log(["backup"], `Statusdump fetched, turn is ${statusDumpWrapper.turnNbr}`);
 
-            else return Promise.resolve();
-        })
-        .then(() => nextPromise());
-    })
+    if (preexecRegex.test(type) === true)
+        return _writeTurnNbrToTmpFile(gameName, statusDumpWrapper.turnNbr, tmpTurnFilePath);
+        
+    else if (postexecRegex.test(type) === true)
+        return _readTurnNbrFromTmpFile(gameName, fetchedStatusDump, tmpTurnFilePath);
+})
+.then(() => _createDirectories(`${targetBackupDir}/Turn ${fetchedStatusDump.turnNbr}`))
+.then(() => 
+{
+    rw.log(["backup"], `Backup directory created at ${targetBackupDir}/Turn ${fetchedStatusDump.turnNbr}`);
+    return fsp.readdir(savedgamesPath);
+})
+.then((filenames) => 
+{
+    rw.log(["backup"], `Read filenames, backing up files...`);
+    _backupFiles(filenames, savedgamesPath, `${targetBackupDir}/Turn ${fetchedStatusDump.turnNbr}`)
+})
+.then(() =>
+{
+    rw.log(["backup"], `Finished backing up turn files, cleaning old ones...`);
+    return _cleanUnusedBackups(fetchedStatusDump);
+})
+.catch((err) => rw.log(["backup", "error"], `Error occurred during backup: ${err.message}\n\n${err.stack}`));
+
+
+
+function _writeTurnNbrToTmpFile(gameName, turnNbr, path)
+{
+    return fsp.writeFile(path, turnNbr, "utf-8")
     .then(() =>
     {
-        //delete previous backups according to the configuration
-        if (turnInfo.turn > config.nbrOfTurnsBackedUp)
-        {
-            if (fs.existsSync(`${target}/Turn ${turnInfo.turn - config.nbrOfTurnsBackedUp}`) === false)
-            return Promise.resolve();
-
-            return rw.atomicRmDir(`${target}/Turn ${turnInfo.turn - config.nbrOfTurnsBackedUp}`);
-        }
-
-        else return Promise.resolve();
+        rw.log(["backup"], `Preexec wrote turn ${turnNbr} to file.`);
+        return Promise.resolve();
+    })
+    .catch((err) =>
+    {
+        rw.log(["backup"], `Preexec could not write turn ${turnNbr} to file: ${err.message}\n\n${err.stack}`);
+        return Promise.resolve();
     });
-});
+}
 
+function _readTurnNbrFromTmpFile(gameName, statusDump, path)
+{
+    return fsp.readFile(path, "utf-8")
+    .then((turnNbrString) =>
+    {
+        rw.log(["backup"], `Postexec read turn ${turnNbrString} from file.`);
+        statusDump.turnNbr = +turnNbrString + 1;
+        return fsp.unlink(`${config.tmpDownloadPath}/${gameName}`);
+    })
+    .catch((err) => rw.log(["backup"], `Postexec error when reading turn from file: ${err.message}\n\n${err.stack}`));
+}
 
-function _createDirectories(target)
+function _createDirectories(targetBackupDir)
 {
     //Linux base paths begin with / so ignore the first empty element
-    if (target.indexOf("/") === 0)
-        target = target.slice(1);
+    if (targetBackupDir.indexOf("/") === 0)
+        targetBackupDir = targetBackupDir.slice(1);
 
-    var directories = target.split("/");
+    var directories = targetBackupDir.split("/");
     var currentPath = directories.shift();
 
     if (process.platform === "linux")
         currentPath = `/${currentPath}`;
 
     if (fs.existsSync(currentPath) === false)
-        throw new Error(`The base path ${currentPath} specified for the backup target does not exist.`);
+        return Promise.reject(new Error(`The base path ${currentPath} specified for the backup target does not exist.`));
 
     return directories.forEachPromise((dir, index, nextPromise) =>
     {
@@ -106,5 +123,47 @@ function _createDirectories(target)
         }
             
         else return nextPromise();
-    });
+    })
+    .catch((err) => Promise.reject(err));
+}
+
+function _backupFiles(filenames, sourcePath, targetPath)
+{
+    return filenames.forEachPromise((filename, index, nextPromise) =>
+    {
+        rw.log(["backup"], `Checking ${filename}...`);
+        
+        if (extensionsToBackupRegex.test(filename) === false)
+        {
+            rw.log(["backup"], `Not a turn file; skipping.`);
+            return nextPromise();
+        }
+
+        rw.log(["backup"], `Turn file found, backing up...`);
+
+        return rw.copyFile(`${sourcePath}/${filename}`, `${targetPath}/${filename}`)
+        .then(() =>
+        {
+            rw.log(["backup"], `Turn file backed up.`);
+            return nextPromise();
+        })
+        .catch((err) => Promise.reject(err));
+    })
+    .catch((err) => Promise.reject(err));
+}
+
+function _cleanUnusedBackups(statusDump)
+{
+    // If turn number is lower than number of backups to keep, no need to clean
+    if (statusDump.turnNbr <= config.nbrOfTurnsBackedUp)
+    {
+        rw.log(["backup"], `No old turn backups to clean.`);
+        return Promise.resolve();
+    }
+
+    // Otherwise, delete previous backups according to the configuration
+    const turnNbrToClean = statusDump.turnNbr - config.nbrOfTurnsBackedUp;
+
+    return cleaner.deleteBackupsUpToTurn(targetBackupDir, turnNbrToClean)
+    .catch((err) => Promise.reject(err));
 }
--- unused_files_cleaner.js
+++ unused_files_cleaner.js
@@ -2,6 +2,64 @@
 const fs = require("fs");
 const fsp = require("fs").promises;
 const config = require("./config.json");
+const rw = require("./reader_writer.js");
+
+module.exports.deleteAllTurnBackups = function(gameName)
+{
+    const target = `${config.dataFolderPath}/backups/${gameName}`;
+    const pathToNewTurnBackups = `${target}/${config.newTurnsBackupDirName}`;
+    const pathTPreHostBackups = `${target}/${config.preHostTurnBackupDirName}`;
+
+    return exports.deleteBackupsUpToTurn(pathToNewTurnBackups, Infinity)
+    .then(() => exports.deleteBackupsUpToTurn(pathTPreHostBackups, Infinity))
+    .then(() =>
+    {
+        rw.log(["backup"], `All of ${gameName}'s turn backups were cleaned.`);
+        return Promise.resolve();
+    })
+    .catch((err) => 
+    {
+        rw.log(["backup"], `Could not clean all turn backups: ${err.message}\n\n${err.stack}`);
+        return Promise.reject(new Error(`Could not clean all turn backups: ${err.message}`))
+    });
+};
+
+module.exports.deleteBackupsUpToTurn = function(dirPath, turnNbrToClean)
+{
+    // Get filenames of all saved turn directories in the backup path
+    return rw.getDirFilenames(dirPath)
+    .then((filenames) =>
+    {
+        return filenames.forEachPromise((filename, i, nextPromise) =>
+        {
+            return fsp.stat(`${dirPath}/${filename}`)
+            .then((stats) =>
+            {
+                const dirTurnNbr = +filename.replace(/\D*/g, "");
+
+                if (stats.isDirectory() === false)
+                    return nextPromise();
+
+                // Folder of a backup from recent turn, do not clean
+                if (dirTurnNbr > turnNbrToClean)
+                    return nextPromise();
+
+                // Folders of turns at the number given or before will be deleted
+                return rw.deleteDir(`${dirPath}/${filename}`)
+                .then(() => 
+                {
+                    rw.log(["backup"], `Cleaned backup of turn ${dirTurnNbr}.`);
+                    return nextPromise();
+                })
+                .catch((err) => 
+                {
+                    rw.log(["backup"], `Could not clean ${filename}: ${err.message}\n\n${err.stack}`);
+                    return nextPromise();
+                });
+            });
+        });
+    });
+};
 
 module.exports.deleteUnusedMaps = function(mapsInUse)
 {
