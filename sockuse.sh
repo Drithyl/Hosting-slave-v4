f344b04be8fc1ff9247c51c71de612d94e802471
Drithyl <hugo.bujan@gmail.com>
2021-12-12 23:58:22 -0500

    Bash script to check if ports are in use

---
+++ sockuse.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+port=$1
+
+#-atn will NOT resolve some ports to service names
+#with just -at, some relevant ports will not be parsable
+#as they will appear as a name. For example, a dom5 process
+#might show as 0.0.0.0:cslistener, instead of 0.0.0.0:9000
+if [ $(ss -atn|grep -c $port) = '0' ]; then
+	echo "0"
+else
+	echo "1"
+fi
+
+#by pencils, 2021
+#modified by Drithyl to use ss -atn instead of ss -at, 2021
