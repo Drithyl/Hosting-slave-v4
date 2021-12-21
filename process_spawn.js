25724dd9a7cfcefd32a9b2c9d466c4854dea9931
Drithyl <hugo.bujan@gmail.com>
2021-12-20 21:00:56 -0500

    Game processes now pipe their stdio to dated files

--- process_spawn.js
+++ process_spawn.js
@@ -1,17 +1,21 @@
 
 const fs = require("fs");
+const path = require("path");
 const log = require("./logger.js");
 const assert = require("./asserter.js");
+const rw = require("./reader_writer.js");
 const configStore = require("./config_store.js");
 const spawn = require('child_process').spawn;
 
 const REPETITIVE_DATA_DEBOUNCER_INTERVAL = 600000;
 const DOM5_EXE_PATH = configStore.dom5ExePath;
+const BASE_LOG_PATH = path.resolve(configStore.dataFolderPath, "logs", "games");
 const MISSING_EXE_MESSAGE = `The dom5 executable path ${DOM5_EXE_PATH} does not exist!`;
 
 
 if (fs.existsSync(DOM5_EXE_PATH) === false)
 	throw new Error(MISSING_EXE_MESSAGE);
+
 
 
 module.exports.SpawnedProcessWrapper = SpawnedProcessWrapper;
@@ -25,11 +29,13 @@ function SpawnedProcessWrapper(gameName, args, onSpawned)
 	if (fs.existsSync(DOM5_EXE_PATH) === false)
 		throw new Error(MISSING_EXE_MESSAGE);
 
-
 	const _name = gameName;
 	const _args = args;
 	const _recentDataEmitted = [];
 	const _onSpawned = onSpawned;
+	const _stdoutLogPath = path.resolve(BASE_LOG_PATH, _name);
+	const _stderrLogPath = path.resolve(BASE_LOG_PATH, _name);
+
 	var _spawnedSuccessfully = false;
 	var _onError;
 	var _onExit;
@@ -44,7 +50,9 @@ function SpawnedProcessWrapper(gameName, args, onSpawned)
 	// This makes the games remain running but unable to be connected, hanging at "Waiting for info..."
 	// https://nodejs.org/api/stream.html#stream_class_stream_readable
 	// stdio array is [stdin, stdout, stderr]
-	const _instance = spawn(DOM5_EXE_PATH, _args, { stdio: ["ignore", "pipe", "pipe"] });
+	const _instance = spawn(DOM5_EXE_PATH, _args, { 
+		stdio: ["pipe", "pipe", "pipe"]
+	});
 
 	_instance.onProcessError = (handler) => _onError = handler;
 	_instance.onProcessExit = (handler) => _onExit = handler;
@@ -56,6 +64,8 @@ function SpawnedProcessWrapper(gameName, args, onSpawned)
 	_instance.on("spawn", () =>
 	{
 		_spawnedSuccessfully = true;
+		_pipeToLog(_instance.stdout, _stdoutLogPath, "stdout.txt");
+		_pipeToLog(_instance.stderr, _stderrLogPath, "stderr.txt");
 		onSpawned();
 	});
 
@@ -80,16 +90,16 @@ function SpawnedProcessWrapper(gameName, args, onSpawned)
 			_onClose(code, signal)
 	});
 
-	_instance["stderr"].setEncoding("utf8");
-	_instance["stderr"].on("data", (data) => 
+	_instance.stderr.setEncoding("utf8");
+	_instance.stderr.on("data", (data) => 
 	{
 		if (assert.isFunction(_onStderr) === true)
 			if (_isRelevantData(_recentDataEmitted, data) === true)
 				_onStderr(data)
 	});
 
-	_instance["stdout"].setEncoding("utf8");
-	_instance["stdout"].on("data", (data) => 
+	_instance.stdout.setEncoding("utf8");
+	_instance.stdout.on("data", (data) => 
 	{
 		if (assert.isFunction(_onStdout) === true)
 			if (_isRelevantData(_recentDataEmitted, data) === true)
@@ -99,6 +109,15 @@ function SpawnedProcessWrapper(gameName, args, onSpawned)
 	return _instance;
 }
 
+// Pipes one of the child's streams to a given file with the current date attached;
+// This will create new files every day so they are easy to go and read from
+async function _pipeToLog(readable, dirPath, filename)
+{
+	const date = new Date();
+	const finalPath = path.resolve(dirPath, `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}-${filename}`);
+	await rw.checkAndCreateFilePath(finalPath);
+	readable.pipe(fs.createWriteStream(finalPath, { flags: "a" }));
+}
 
 function _isRelevantData(recentDataEmitted, stdioData)
 {
