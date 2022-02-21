a7e80fe3f33e5ab9b93f7bcb71a1b7b86966102c
Drithyl <hugo.bujan@gmail.com>
2022-02-20 21:17:51 -0500

    Rewritten to effectively be an interface

--- socket_wrapper.js
+++ socket_wrapper.js
@@ -1,12 +1,10 @@
 
 const configStore = require("./config_store.js");
+const socket = (configStore.useWs === true) ? require("./ws_wrapper.js") : require("./socket_io_wrapper.js");
 
-if (configStore.useWs === true)
-{
-    module.exports = require("./ws_wrapper.js");
-}
 
-else
-{
-    module.exports = require("./socket_io_wrapper.js");
-}
+module.exports.connect = (...args) => socket.connect(...args);
+module.exports.on = (...args) => socket.on(...args);
+module.exports.emit = (...args) => socket.emit(...args);
+module.exports.emitPromise = (...args) => socket.emitPromise(...args);
+module.exports.connect = (...args) => socket.connect(...args);
