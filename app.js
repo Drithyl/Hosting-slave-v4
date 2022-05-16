6ceb46885da3e685c0fe277866d53592674ec5a5
Hugo Buján Prévot <hugo.bujan@gmail.com>
2022-05-15 20:08:51 -0400

    Refactored initialization to use async/await

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ configStore.createConfig()
 });
 
 
-function _initializeComponents()
+async function _initializeComponents()
 {
     const fs = require("fs");
     const log = require("./logger.js");
@@ -24,15 +24,25 @@ function _initializeComponents()
         fs.mkdirSync(`${configStore.dataFolderPath}/backups`);
 
 
-    // Get the initial statusdump of all games, so that there is
-    // never a time where the master connects here and there are
-    // no statuses ready to be read from
-    statusStore.populate()
-    .then(() => statusStore.startUpdateCycle())
-    .then(() => oldFilesCleaner.startBackupCleanInterval())
-    .then(() => oldFilesCleaner.startLogCleanInterval())
-    .then(() => socketWrapper.connect())
-    .catch((err) => log.error(log.getLeanLevel(), `INITIALIZATION ERROR`, err));
+    try
+    {
+        // Get the initial statusdump of all games, so that there is
+        // never a time where the master connects here and there are
+        // no statuses ready to be read from
+        await statusStore.populate();
+
+        statusStore.startUpdateCycle();
+        oldFilesCleaner.startBackupCleanInterval();
+        oldFilesCleaner.startLogCleanInterval();
+
+        await socketWrapper.connect();
+    }
+    
+    catch(err)
+    {
+        log.error(log.getLeanLevel(), `INITIALIZATION ERROR`, err)
+    }
+    
 
     process.on("error", (err) => log.error(log.getLeanLevel(), `PROCESS ERROR`, err));
     process.on("unhandledRejection", err => log.error(log.getLeanLevel(), `UNHANDLED REJECTION ERROR`, err));
